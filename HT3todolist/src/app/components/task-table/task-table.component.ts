1cc2d903911b499950306c23fc504eb391071cf5
Maks Krasnikov <uw5ekr@gmail.com>
2020-07-29 21:20:32 +0300

    modified task component

--- HT3todolist/src/app/components/task-table/task-table.component.ts
+++ HT3todolist/src/app/components/task-table/task-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'task-table',
@@ -7,15 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskTableComponent implements OnInit {
 
-  elements: any = [
+  /*elements: any = [
     {id: 1, first: 'Mark', last: 'Otto', handle: '@mdo'},
     {id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat'},
     {id: 3, first: 'Larry', last: 'the Bird', handle: '@twitter'},
-  ];
+  ];*/
 
-  headElements = ['ID', 'First', 'Last', 'Handle'];
+  elements = [];
+  headElements = ['ID', 'Task name', 'Description', 'Data', 'Begin time', 'End time', 'Is complieted?', 'Action'];
 
-  constructor() { }
+  constructor(public tasksService: TasksService ) {
+    this.elements = tasksService as any;
+    console.log(this.elements);
+  }
 
   ngOnInit(): void {
   }
